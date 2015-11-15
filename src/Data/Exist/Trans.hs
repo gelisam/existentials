5a70a240c0e9e8c41f5f2d28267f56049b6a0429
Samuel Gélineau <gelisam+github@gmail.com>
2015-11-15 13:22:28 -0500

    existential over the last type argument

--- src/Data/Exist/Trans.hs
+++ src/Data/Exist/Trans.hs
@@ -1,5 +1,25 @@
+{-# LANGUAGE GADTs #-}
 module Data.Exist.Trans where
 
 -- |
--- >>> 2+2
--- 4
+-- Hide the last argument of a type constructor.
+-- Since the type information is lost, we can't print values of that type
+-- nor do anything else with those values.
+-- 
+-- >>> let x = Some (Just 3) :: Some Maybe
+-- >>> :{
+-- case x of
+--   Some (Just _) -> "Just _"
+--   Some Nothing  -> show Nothing
+-- :}
+-- "Just _"
+-- 
+-- >>> let x = Some (Right 3) :: Some (Either String)
+-- >>> :{
+-- case x of
+--   Some (Left  s) -> show (Left s)
+--   Some (Right _) -> "Right _"
+-- :}
+-- "Right _"
+data Some f where
+    Some :: f a -> Some f
