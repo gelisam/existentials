b620b00d3f45414e014a06ad627bd8e106c22fa9
Samuel Gélineau <gelisam+github@gmail.com>
2015-11-16 22:12:46 -0500

    uniform examples

--- src/Data/Exist/Trans.hs
+++ src/Data/Exist/Trans.hs
@@ -28,17 +28,31 @@ import Data.Functor.Identity
 -- 
 -- Here are `exists a. Maybe a` and `exists a. Either String a`:
 -- 
+--   >>> type MaybeX = Some Maybe
 --   >>> :{
---   let x :: Some Maybe
---       x = Some (Just 3)
---   in unSome blankFunctor x
+--   let mkMaybeX :: Maybe a -> MaybeX
+--       mkMaybeX = Some
+--       --
+--       unMaybeX :: (forall a. Maybe a -> r) -> MaybeX -> r
+--       unMaybeX cc = unSome $ cc
+--       --
+--       myMaybeX :: MaybeX
+--       myMaybeX = mkMaybeX (Just 3)
+--   in unMaybeX blankFunctor myMaybeX
 --   :}
 --   Just ()
 --   
+--   >>> type EitherStringX = Some (Either String)
 --   >>> :{
---   let x :: Some (Either String)
---       x = Some (Right 3)
---   in unSome blankFunctor x
+--   let mkEitherStringX :: Either String a -> EitherStringX
+--       mkEitherStringX = Some
+--       --
+--       unEitherStringX :: (forall a. Either String a -> r) -> EitherStringX -> r
+--       unEitherStringX cc = unSome $ cc
+--       --
+--       myEitherStringX :: EitherStringX
+--       myEitherStringX = mkEitherStringX (Right 3)
+--   in unEitherStringX blankFunctor myEitherStringX
 --   :}
 --   Right ()
 -- 
@@ -49,17 +63,35 @@ import Data.Functor.Identity
 --   -- type mismatch
 --   -- >>> Some [(), "foo"] :: Some []
 --   
---   >>> unSome blankFunctor (Some [3, 4] :: Some [])
+--   >>> type ListX = Some []
+--   >>> :{
+--   let mkListX :: [a] -> ListX
+--       mkListX = Some
+--       --
+--       unListX :: (forall a. [a] -> r) -> ListX -> r
+--       unListX cc = unSome $ cc
+--       --
+--       myListX :: ListX
+--       myListX = mkListX [3, 4]
+--   in unListX blankFunctor myListX
+--   :}
 --   [(),()]
 -- 
 -- To approximate `exists a. a`, use `Some Identity`:
 -- 
+--   >>> type X = Some Identity
 --   >>> :{
---   let xs :: [Some Identity]
---       xs = [ Some (Identity 3)
---            , Some (Identity "foo")
---            ]
---   in fmap (unSome (runIdentity . blankFunctor)) xs
+--   let mkX :: a -> X
+--       mkX = Some . Identity
+--       --
+--       unX :: (forall a. a -> r) -> X -> r
+--       unX cc = unSome (cc . runIdentity)
+--       --
+--       myXs :: [X]
+--       myXs = [ mkX 3
+--              , mkX "foo"
+--              ]
+--   in fmap (unX blank) myXs
 --   :}
 --   [(),()]
 data Some f where
@@ -76,15 +108,18 @@ unSome cc (Some x) = cc x
 -- 
 --   >>> type EitherXX = SomeT Some Either
 --   >>> :{
---   let mkFoo :: Either a b -> EitherXX
---       mkFoo = SomeT . Some
+--   let mkEitherXX :: Either a b -> EitherXX
+--       mkEitherXX = SomeT . Some
 --       --
---       unFoo :: (forall a b. Either a b -> r) -> EitherXX -> r
---       unFoo cc = unSomeT $ unSome $ cc
+--       unEitherXX :: (forall a b. Either a b -> r) -> EitherXX -> r
+--       unEitherXX cc = unSomeT $ unSome $ cc
 --       --
---       myFoo :: [EitherXX]
---       myFoo = [mkFoo (Left 1), mkFoo (Left "foo"), mkFoo (Right 42)]
---   in map (unFoo blankBifunctor) myFoo
+--       myEitherXXs :: [EitherXX]
+--       myEitherXXs = [ mkEitherXX (Left 1)
+--                     , mkEitherXX (Left "foo")
+--                     , mkEitherXX (Right 42)
+--                     ]
+--   in map (unEitherXX blankBifunctor) myEitherXXs
 --   :}
 --   [Left (),Left (),Right ()]
 data SomeT t f where
